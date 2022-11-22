71659b42d28e1b1fc111c58505ef52f2a38f92e2
Kelsey Selvaggi <kselvaggi13@gmail.com>
2022-11-22 00:09:57 -0500

    added codewars problems

--- homework.js
+++ homework.js
@@ -55,3 +55,63 @@ replaceEvens(["Max","Baseball","Reboot","Goku","Trucks","Rodger"])
 //Expected output
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 //Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
+
+
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// Note: for this kata y isn't considered a vowel.
+// https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
+
+// def disemvowel(string_):
+//     vowels = ['a', 'A', 'E', 'I', 'O', 'U', 'e', 'i', 'o', 'u']
+//     result = ''.join([l for l in string_ if l not in vowels])
+//     return result
+
+
+function disemvowel(str) {
+    let arr = str.split('');
+    for (let i = 0; i < arr.length; i++){
+      if ('aeiou'.includes(arr[i].toLowerCase())){
+        arr.splice(i, 1)
+        i--
+      }
+    }
+    return arr.join('');
+  }
+
+console.log(disemvowel("This website is for losers LOL!"));
+console.log(disemvowel("No offense but,\nYour writing is among the worst I've ever read"))
+console.log(disemvowel("What are you, a communist?"))
+
+
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+// move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+
+// def move_zeros(lst):
+//     result = []
+//     for item in lst:
+//         if item != 0:
+//             result.append(item)
+//     diff = len(lst) - len(result)
+//     for i in range(diff):
+//         result.append(0)
+//     return result
+
+function moveZeros(arr) {
+    for (let i = arr.length - 1; i >= 0; i--){
+      if (arr[i] == 0){
+        arr.splice(i, 1);
+        arr.push(0);
+      }
+    }
+    return arr;
+}
+
+  console.log(moveZeros([1,2,0,1,0,1,0,3,0,1]))
